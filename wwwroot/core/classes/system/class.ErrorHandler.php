206f968b9de3637fd79791fc6546a79ce37c7351
Lucas Vienna <lmvienna@outlook.com>
2019-01-30 11:46:15 +0100

    added proper HTTP request support to the `register` handler

--- wwwroot/core/classes/system/class.ErrorHandler.php
+++ wwwroot/core/classes/system/class.ErrorHandler.php
@@ -2,27 +2,27 @@
 
 abstract class ErrorID
 {
-    const FAILED_LOGIN        = "fail_login";
-    const ACCESS_DENIED       = "access_denied";
-    const EMAIL_COMPLETE      = "email_confirmed";
-    const EMAIL_MISSING       = "email_missing";
+    const FAILED_LOGIN = "fail_login";
+    const ACCESS_DENIED = "access_denied";
+    const EMAIL_COMPLETE = "email_confirmed";
+    const EMAIL_MISSING = "email_missing";
     const VERIFICATION_FAILED = "verification_failed_invalid_code";
-    const REDEEMED_CODE       = "redeem_code";
-    const DUPLICATE_CODE      = "duplicate_redeem";
-    const LOGGED_OUT          = 'logged_out';
+    const REDEEMED_CODE = "redeem_code";
+    const DUPLICATE_CODE = "duplicate_redeem";
+    const LOGGED_OUT = 'logged_out';
 }
 
 abstract class ErrorMessages
 {
-    const FAILED_LOGIN_MESSAGE           = "Your Login failed! Check your login data or try the forgot password function!";
-    const EMAIL_VERIFICATION_MISSING     = "Your E-Mail isn't confirmed yet! We send you a E-Mail, check your Spam folder!";
+    const FAILED_LOGIN_MESSAGE = "Your Login failed! Check your login data or try the forgot password function!";
+    const EMAIL_VERIFICATION_MISSING = "Your E-Mail isn't confirmed yet! We send you a E-Mail, check your Spam folder!";
     const EMAIL_VERIFICATION_FAILED_CODE = "Your Code is not valid!";
-    const EMAIL_COMPLETE                 = "Your E-Mail is now confirmed! Thank you! Go ahead and login.";
-    const ACCESS_DENIED                  = "You don't have access to this area!";
-    const UNKNOWN_ERROR                  = "Unknown Error happend!";
-    const REDEEMED_CODE_MESSAGE          = "Congratulations! You've redeemed this code! Enjoy!";
-    const DUPLICATE_REDEEM_MESSAGE       = "You may not redeem the same code more than once. Stop cheating!";
-    const LOGGET_OUT_MESSAGE             = "Thanks for playing! We hope to see you soon!";
+    const EMAIL_COMPLETE = "Your E-Mail is now confirmed! Thank you! Go ahead and login.";
+    const ACCESS_DENIED = "You don't have access to this area!";
+    const UNKNOWN_ERROR = "Unknown Error happend!";
+    const REDEEMED_CODE_MESSAGE = "Congratulations! You've redeemed this code! Enjoy!";
+    const DUPLICATE_REDEEM_MESSAGE = "You may not redeem the same code more than once. Stop cheating!";
+    const LOGGET_OUT_MESSAGE = "Thanks for playing! We hope to see you soon!";
 }
 
 class ErrorHandler
@@ -44,9 +44,9 @@ class ErrorHandler
     public function add($ID, $Title, $Message)
     {
         $error = [
-            "ID"      => $ID,
+            "ID" => $ID,
             "MESSAGE" => $Message,
-            "TITLE"   => $Title,
+            "TITLE" => $Title,
         ];
         array_push($this->errors_, $error);
     }
@@ -58,7 +58,7 @@ class ErrorHandler
     {
         if (isset($_GET["error_id"]) && !empty($_GET["error_id"])) {
             $this->isError = true;
-            $ID            = $_GET["error_id"];
+            $ID = $_GET["error_id"];
         } else {
             $ID = "";
         }
@@ -66,7 +66,7 @@ class ErrorHandler
         $this->error = $ID;
         foreach ($this->errors_ as $error) {
             if ($error['ID'] == $ID) {
-                $this->error_msg   = $error['MESSAGE'];
+                $this->error_msg = $error['MESSAGE'];
                 $this->error_title = $error['TITLE'];
             }
         }
--- wwwroot/register.php
+++ wwwroot/register.php
@@ -24,56 +24,53 @@ foreach ($PARAMS as $FIELD_NAME => $PARAM) {
 
 //MISSING PARAMETERS
 if (!empty($MISSING)) {
-    die(json_encode(["error" => true, "error_msg" => "Fill out " . implode(', ', $MISSING) . "!"]));
+    http_response_code(400);
+    die(json_encode(["message" => "Fill out " . implode(', ', $MISSING) . "!"]));
 }
 
 //VALIDATE GOOGLE_RECAPTCHA
 $RECAPTCHA_RESPONSE = json_decode(file_get_contents('https://www.google.com/recaptcha/api/siteverify?secret=' . GOOGLE_CAPTCHA_KEY . '&response=' . $_POST['g-recaptcha-response']));
 if (!$RECAPTCHA_RESPONSE->success) {
-    die(json_encode(["error" => true, "error_msg" => "Check your ReCaptcha!"]));
+    http_response_code(400);
+    die(json_encode(["message" => "Check your ReCaptcha!"]));
 }
 
 //VALIDATE USERNAME
 if (strlen($_POST['registerUsername']) < USERNAME_MIN_LENGTH) {
-    die(json_encode([
-        "error" => true,
-        "error_msg" => "Your chosen username is to short! Min length: " . USERNAME_MIN_LENGTH,
-    ]));
+    http_response_code(400);
+    die(json_encode(["message" => "Your chosen username is to short! Min length: " . USERNAME_MIN_LENGTH]));
 }
 
 if (strlen($_POST['registerUsername']) > USERNAME_MAX_LENGTH) {
-    die(json_encode([
-        "error" => true,
-        "error_msg" => "Your chosen username is to long! Max length: " . USERNAME_MAX_LENGTH,
-    ]));
+    http_response_code(400);
+    die(json_encode(["message" => "Your chosen username is to long! Max length: " . USERNAME_MAX_LENGTH]));
 }
 
 if (!$System->validate->isValidString($_POST['registerUsername'], ALLOWED_CHARS)) {
-    die(json_encode([
-        "error" => true,
-        "error_msg" => "Your username can only contain [a-Z][0-9][" . implode(',', ALLOWED_CHARS) . "]!",
-    ]));
+    http_response_code(400);
+    die(json_encode(["message" => "Your username can only contain [a-Z][0-9][" . implode(',', ALLOWED_CHARS) . "]!"]));
 }
 
 if ($System->validate->isUserByUsername($_POST['registerUsername'])) {
-    die(json_encode(["error" => true, "error_msg" => "Your chosen username is already taken!"]));
+    http_response_code(400);
+    die(json_encode(["message" => "Your chosen username is already taken!"]));
 }
 
 //VALIDATE PASSWORD
 if (strlen($_POST['registerPassword']) < PASSWORD_MIN_LENGTH) {
-    die(json_encode([
-        "error" => true,
-        "error_msg" => "Your chosen password is to short! Min length: " . PASSWORD_MIN_LENGTH,
-    ]));
+    http_response_code(400);
+    die(json_encode(["message" => "Your chosen password is to short! Min length: " . PASSWORD_MIN_LENGTH]));
 }
 
 if ($_POST['registerPassword'] == $_POST['registerUsername']) {
-    die(json_encode(["error" => true, "error_msg" => "Your Username shouldnt be your password..."]));
+    http_response_code(400);
+    die(json_encode(["message" => "Your username shouldn't be your password..."]));
 }
 
 //VALIDATE EMAIL
 if (!filter_var($_POST['registerEmail'], FILTER_VALIDATE_EMAIL)) {
-    die(json_encode(["error" => true, "error_msg" => "Your E-Mail is invalid!"]));
+    http_response_code(400);
+    die(json_encode(["message" => "Your E-Mail is invalid!"]));
 }
 
 if (
@@ -82,24 +79,28 @@ if (
         'freemail.hu') !== false || strpos($_POST['registerEmail'],
         'outlook.de') !== false || strpos($_POST['registerEmail'], 'outlook.com') !== false
 ) {
-    die(json_encode([
-        "error" => true,
-        "error_msg" => "We can't support hotmail.com , hotmail.de,  freemail.hu, outlook.de, outlook.com e-mails!",
-    ]));
+    http_response_code(400);
+    die(json_encode(["message" => "We can't support hotmail.com , hotmail.de,  freemail.hu, outlook.de, outlook.com e-mails!"]));
 }
 
 if ($System->validate->isUserByEmail($_POST['registerEmail'])) {
-    die(json_encode(["error" => true, "error_msg" => "Your E-Mail is already taken! May try forgot password!"]));
+    http_response_code(400);
+    die(json_encode(["message" => "Your E-Mail is already taken! May try forgot password!"]));
 }
 
 //CHECK INVATION CODE
 if (NEED_INVITATION) {
     if (!$System->validate->isValidInvitation($_POST['registerInvation'])) {
-        die(json_encode(["error" => true, "error_msg" => "You aren't invited to this Server!"]));
+        http_response_code(400);
+        die(json_encode(["message" => "Invalid invitation code!"]));
     }
 }
 
 if ($System->registerUser($_POST['registerUsername'], $_POST['registerPassword'], $_POST['registerEmail'])) {
     $System->useInvitationCode($_POST['registerInvation']);
-    die(json_encode(["error" => false, "msg" => "You sucessfully registered!"]));
+    http_response_code(201);
+    die(json_encode(["message" => "You sucessfully registered!"]));
+} else {
+    http_response_code(500);
+    die(json_encode(["message" => "Registration failed, try again later!"]));
 }
