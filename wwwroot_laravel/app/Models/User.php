69b04e1c2c9008d461c15eea70f155a206320e62
Martin Schneider <martin.s99@gmx.net>
2019-06-04 19:13:34 +0200

    Mass-Assign fillables, Genders, Todos

--- wwwroot_laravel/app/Models/User.php
+++ wwwroot_laravel/app/Models/User.php
@@ -6,9 +6,20 @@ use Illuminate\Notifications\Notifiable;
 use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 
+/** TODO: Change "SESSION_ID" to "remember_token" in table for laravel's authentication system **/
+/** TODO: Migrate GG, Settings, etc. **/
+
+
 class User extends Authenticatable
 {
     use Notifiable;
+
+    public static $GENDER = [
+        '1' => 'Male',
+        '2' => 'Female',
+        '3' => 'Potato',
+        '4' => 'Other',
+    ];
 
     /**
      * The attributes that are mass assignable.
@@ -16,7 +27,10 @@ class User extends Authenticatable
      * @var array
      */
     protected $fillable = [
-        'name', 'email', 'password',
+        'USER_ID', 'USERNAME', 'EMAIL', 'SESSION_ID', 'PW_HASH', 'LAST_SERVER', 'IP', 'VERFIED',
+        'ONLINE', 'WAS_TESTER', 'FIRST_NAME', 'LAST_NAME', 'COUNTRY_NAME', 'GENDER',
+        'BIRTHDATE', 'LANGUAGE', 'DISCORD_ID'
+
     ];
 
     /**
